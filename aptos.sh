1fb3c138c60c9459d85f990285dfcf29a2be420a
MrN1x0n <95374645+MrN1x0n@users.noreply.github.com>
2022-05-14 23:02:57 +0300

    Update aptos.sh

--- aptos.sh
+++ aptos.sh
@@ -4,67 +4,66 @@ exists()
   command -v "$1" >/dev/null 2>&1
 }
 if exists curl; then
-	echo ''
+echo ''
 else
-  sudo apt install curl -y < "/dev/null"
+  sudo apt update && sudo apt install curl -y < "/dev/null"
 fi
-curl -s https://raw.githubusercontent.com/MrN1x0n/MrN1x0n/main/logo.sh | bash && sleep 2
-curl -s https://raw.githubusercontent.com/MrN1x0n/aptos/main/swap8.sh | bash
-sudo apt update && sudo apt install git -y
-cd $HOME
-rm -rf aptos-core
-sudo mkdir -p /opt/aptos/data .aptos/config .aptos/key
-git clone https://github.com/aptos-labs/aptos-core.git
-cd aptos-core
-git checkout origin/devnet &>/dev/null
-echo y | ./scripts/dev_setup.sh
-source ~/.cargo/env
-cargo build -p aptos-node --release
-cargo build -p aptos-operational-tool --release
-mv  ~/aptos-core/target/release/aptos-node /usr/local/bin
-mv  ~/aptos-core/target/release/aptos-operational-tool /usr/local/bin
-#/usr/local/bin/aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file ~/.aptos/key/private-key.txt
-if [ -f ~/.aptos/key/private-key.txt ]; then
-    echo ""
-else 
-    /usr/local/bin/aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file ~/.aptos/key/private-key.txt
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
 fi
-/usr/local/bin/aptos-operational-tool extract-peer-from-file --encoding hex --key-file ~/.aptos/key/private-key.txt --output-file ~/.aptos/config/peer-info.yaml &>/dev/null
-cp ~/aptos-core/config/src/config/test_data/public_full_node.yaml ~/.aptos/config
-wget -O /opt/aptos/data/genesis.blob https://devnet.aptoslabs.com/genesis.blob
-wget -q -O ~/.aptos/waypoint.txt https://devnet.aptoslabs.com/waypoint.txt
-wget -q -O /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
-wget -O seeds.yaml https://github.com/MrN1x0n/aptos/raw/main/seeds.yaml
-WAYPOINT=$(cat ~/.aptos/waypoint.txt)
-PRIVKEY=$(cat ~/.aptos/key/private-key.txt)
-PEER=$(sed -n 2p ~/.aptos/config/peer-info.yaml | sed 's/.$//')
-sed -i.bak -e "s/0:01234567890ABCDEFFEDCA098765421001234567890ABCDEFFEDCA0987654210/$WAYPOINT/" $HOME/.aptos/config/public_full_node.yaml
-sed -i "s/genesis_file_location: .*/genesis_file_location: \"\/opt\/aptos\/data\/genesis.blob\"/" $HOME/.aptos/config/public_full_node.yaml
-sleep 2 
-sed -i "s/127.0.0.1/0.0.0.0/" $HOME/.aptos/config/public_full_node.yaml
-sed -i '/network_id: "public"$/a\
-      identity:\
-        type: "from_config"\
-        key: "'$PRIVKEY'"\
-        peer_id: "'$PEER'"' $HOME/.aptos/config/public_full_node.yaml
+sleep 1 && curl -s https://raw.githubusercontent.com/MrN1x0n/MrN1x0n/main/logo.sh | bash && sleep 1
+if [ ! $APTOS_NODENAME ]; then
+read -p "Enter node name: " APTOS_NODENAME
+echo 'export APTOS_NODENAME='\"${APTOS_NODENAME}\" >> $HOME/.bash_profile
+fi
+echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+. $HOME/.bash_profile
 
-/usr/local/bin/yq ea -i 'select(fileIndex==0).full_node_networks[0].seeds = select(fileIndex==1).seeds | select(fileIndex==0)' $HOME/.aptos/config/public_full_node.yaml seeds.yaml
+apt update && apt install git sudo unzip wget -y
+#install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+#install docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+ 
+#install aptos
+wget -qO aptos-cli.zip https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v0.1.1/aptos-cli-0.1.1-Ubuntu-x86_64.zip
+unzip -o aptos-cli.zip
+chmod +x aptos
+mv aptos /usr/local/bin 
+ 
+#create folder,download config    
+IPADDR=$(curl ifconfig.me) 
+sleep 2   
+mkdir -p $HOME/.aptos
+cd $HOME/.aptos
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/docker-compose.yaml
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/validator.yaml
+wget https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/fullnode.yaml
 
-echo "[Unit]
-Description=Aptos
-After=network.target
+aptos genesis generate-keys --output-dir $HOME/.aptos
 
-[Service]
-User=$USER
-Type=simple
-ExecStart=/usr/local/bin/aptos-node -f $HOME/.aptos/config/public_full_node.yaml
-Restart=on-failure
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target" > $HOME/aptosd.service
-mv $HOME/aptosd.service /etc/systemd/system/
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-sudo systemctl enable aptosd
-sudo systemctl restart aptosd
+aptos genesis set-validator-configuration \
+    --keys-dir $HOME/.aptos --local-repository-dir $HOME/.aptos \
+    --username $APTOS_NODENAME \
+    --validator-host $IPADDR:6180 \
+    --full-node-host $IPADDR:6182
+    
+aptos key generate --output-file root_key.txt
+KEYTXT=$(cat ~/.aptos/root_key.txt.pub) 
+KEY="0x"$KEYTXT 
+echo "---
+root_key: \"$KEY\"
+users:
+  - $APTOS_NODENAME
+chain_id: 23" >layout.yaml
+    
+wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.1.0/framework.zip
+unzip -o framework.zip
+aptos genesis generate-genesis --local-repository-dir $HOME/.aptos --output-dir $HOME/.aptos
+sleep 2
+docker compose up -d
+echo -e "Your Aptos node \e[32minstalled and works\e[39m!"
